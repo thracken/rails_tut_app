d0b8de9c646dc988a2b2b53986610ca19f25f5c9
Jeff <jeffgreenland.1@gmail.com>
2016-05-04 13:08:35 -0700

    Updated routes for static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
