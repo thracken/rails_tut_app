087eef667589d52dfe4bfb321c083e2275df26b0
Jeff G <jeffgreenland.1@gmail.com>
2016-06-13 19:38:03 -0700

    Reset database, added passwords to users.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Sample User", email: "user@example.com")
+    @user = User.new(name: "Sample User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "Should be valid" do
@@ -50,5 +50,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present and not blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = " " * 5
+    assert_not @user.valid?
   end
 end
