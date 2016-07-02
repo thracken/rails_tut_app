d6e6aee08a6924843ad6cdebd53fc51a2762d20d
thracken <jeffgreenland.1@gmail.com>
2016-07-01 22:08:05 -0700

    Updated flash message, gravatar function w/size option.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, options = {size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravater_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravater_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravater_url, alt: user.name, class: "gravatar")
   end
 end
