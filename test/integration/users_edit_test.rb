eb98eeaa7d06d9d551699c909a40756780fc294f
thracken <jeffgreenland.1@gmail.com>
2016-08-09 23:48:39 -0700

    Added requirement for users to be logged in before editing an account.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,23 +7,36 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
-    get edit_user_path(@user)
+    log_in_as @user
+    get edit_user_path @user
     assert_template 'users/edit'
     patch user_path(@user),  user: { name:  "", email: "foo@invalid", password: "foo", password_confirmation: "bar" }
-
     assert_template 'users/edit'
   end
 
   test "successful edit" do
-  get edit_user_path(@user)
-  assert_template 'users/edit'
-  name  = "Foo Bar"
-  email = "foo@bar.com"
-  patch user_path(@user), user: { name:  name, email: email, password: "", password_confirmation: "" }
-  assert_not flash.empty?
-  assert_redirected_to @user
-  @user.reload
-  assert_equal name,  @user.name
-  assert_equal email, @user.email
-end
+    log_in_as @user
+    get edit_user_path @user
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name:  name, email: email, password: "", password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
