64c07573cfaaf8f2dc07cd63690ad770479f69f2
Jeff <jeffgreenland.1@gmail.com>
2016-08-10 15:05:00 -0700

    Added admin status to Users, and added ability for admin users to delete other Users.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
   def setup
-    @user       = users(:michael)
+    @user = users(:michael)
     @other_user = users(:archer)
   end
 
@@ -15,6 +15,13 @@ class UsersControllerTest < ActionController::TestCase
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { admin: true }
+    assert_not @other_user.reload.admin?
   end
 
 end
