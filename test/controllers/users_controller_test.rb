c5372b635ad841e833e9279a76c30d99e23a15dc
Jeff <jeffgreenland.1@gmail.com>
2016-08-12 07:18:18 -0700

    Added tests for User index page and controller.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,4 +24,17 @@ class UsersControllerTest < ActionController::TestCase
     assert_not @other_user.reload.admin?
   end
 
+  test "should fail destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      assert_raises(NoMethodError) {delete :destroy, id: @user}
+    end
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
 end
