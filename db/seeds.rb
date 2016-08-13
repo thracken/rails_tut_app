18f5f71f9e6f27bd4824265ffac9324d03919d01
Jeff Greenland <jeffgreenland.1@gmail.com>
2016-08-12 17:48:24 -0700

    Added basic activation email templates.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ User.create!(name:  "Example User", email: "example@railstutorial.org", password
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name, email: email, password: password, password_confirmation: password)
+  User.create!(name:  name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
