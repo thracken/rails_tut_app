266e15497b08c7064e5f0eb160f33f3360f89631
Jeff <jeffgreenland.1@gmail.com>
2016-08-16 11:32:44 -0700

    Added user/micropost associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: {maximum: 255}, format: {with: EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 8}, allow_nil: true
+  has_many :microposts
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
